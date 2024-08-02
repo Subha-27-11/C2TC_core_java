650cf4d30b486990b26cb2753b636a19c1ae5f36
ELCOT <ELCOT@Lenovo>
2024-08-02 15:04:34 +0530

    day1

---
+++ Day1/src/dayone/IfDemo.java
@@ -0,0 +1,12 @@
+package dayone;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		if (x != 5)
+		System.out.println("Value of x is not 5");
+	}
+
+}
