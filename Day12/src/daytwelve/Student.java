6467db2c65faa9cc2034268875eb4316514f66c7
ELCOT <ELCOT@Lenovo>
2024-08-19 22:41:17 +0530

    Day12

---
+++ Day12/src/daytwelve/Student.java
@@ -0,0 +1,48 @@
+package daytwelve;
+
+public class Student {
+		private int rollNo;
+		private String name;
+		private int[] marks;
+		private int noOfSubjects;
+		private float per;
+		
+		public int getRollNo() {
+			return rollNo;
+		}
+
+		public void setRollNo(int rollNo) {
+			this.rollNo = rollNo;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int[] getMarks() {
+			return marks;
+		}
+
+		public int getNoOfSubjects() {
+			return noOfSubjects;
+		}
+
+		public void setNoOfSubjects(int noOfSubjects) {
+			this.noOfSubjects = noOfSubjects;
+		}
+
+		 void setPer(float calculatePercentage) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void setMarks(int[] intArr) {
+			// TODO Auto-generated method stub
+			
+		}
+
+}
---
+++ Day12/src/daytwelve/UncheckedExecption.java
@@ -0,0 +1,10 @@
+package daytwelve;
+
+public class UncheckedExecption {
+	public static void main(String[] args) {
+		int intArray[];
+			intArray = new int[] { 1, 2, 3, 4 };
+			System.out.println(intArray[6]);
+	}
+	}
+
