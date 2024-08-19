9eea51aa87558937604f6d8590c1456cc4d45fdf
ELCOT <ELCOT@Lenovo>
2024-08-19 22:32:00 +0530

    DAY10

---
+++ Day10/src/arrays/JaggedDemo.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class JaggedDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int d[][] = { { 10, 20, 30 }, { 40 }, { 12, 21 }, { 11 } };
+
+		System.out.println("Total Rows in Array d : " + d.length);
+
+		MLArray.printArray(d);
+	}
+	}
